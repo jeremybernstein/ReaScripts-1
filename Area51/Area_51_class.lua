ef66a6e3b41ab9a2c62b3678f894e595fad369c4
Goran Kovac <gorankovacstudio@gmail.com>
2020-04-19 09:21:18 +0200

    Fix, remove block flag if drawing new area

--- Area51/Area_51_class.lua
+++ Area51/Area_51_class.lua
@@ -277,7 +277,7 @@ function Element:track()
     self:update_zone(ZONE_BUFFER)
   end
 
-  BLOCK = CUR_AREA_ZONE or ZONE_BUFFER and true or nil  -- GLOBAL BLOCKING FLAG IF MOUSE IS OVER AREA (ALSO USED TO INTERCEPT LMB CLICK)
+  BLOCK = (not mouse.DRAW_AREA and CUR_AREA_ZONE) or ZONE_BUFFER and true or nil  -- GLOBAL BLOCKING FLAG IF MOUSE IS OVER AREA (ALSO USED TO INTERCEPT LMB CLICK)
   A51_cursor = ZONE_BUFFER and ZONE_BUFFER[1] or CUR_AREA_ZONE
   Change_cursor(A51_cursor)
 end
