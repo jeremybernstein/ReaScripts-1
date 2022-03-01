1a5d393c7ff8719d728b64c530b9f773e84a596c
Jeremy Bernstein <jeremy.d.bernstein@googlemail.com>
2022-03-01 19:06:58 +0100

    nodefer refactor

---
+++ VirtualTrack/Virtual_track_Direct.lua
@@ -0,0 +1,59 @@
+--[[
+ * ReaScript Name: Virtual Tracks (Direct)
+ * Author: Sexan
+ * Licence: GPL v3
+ * REAPER: 6.0
+ * Extensions: None
+ * Version: 0.48
+ * Provides: Modules/*.lua
+--]]
+
+--[[
+ * Changelog:
+ * v0.48 (2022-03-01)
+   + Added new and efficient way to check track visibility (reaper bug is resolved with it)
+   + Added few optimizations
+   + Added proper check if window is in front of reaper (fixes clicking thru windows)
+--]]
+
+local reaper = reaper
+
+package.path = debug.getinfo(1, "S").source:match [[^@?(.*[\/])[^\/]-$]] .. "?.lua;" -- GET DIRECTORY FOR REQUIRE
+
+if not reaper.APIExists("JS_ReaScriptAPI_Version") then
+  reaper.MB( "JS_ReaScriptAPI is required for this script", "Please download it from ReaPack", 0 )
+  return reaper.defer(function() end)
+ else
+   local version = reaper.JS_ReaScriptAPI_Version()
+   if version < 1.002 then
+     reaper.MB( "Your JS_ReaScriptAPI version is " .. version .. "\nPlease update to latest version.", "Older version is installed", 0 )
+     return reaper.defer(function() end)
+   end
+end
+
+require("Modules/VTCommon")
+require("Modules/Class")
+require("Modules/Mouse")
+require("Modules/Utils")
+
+local function Main()
+    local window, segment, details = reaper.BR_GetMouseCursorContext()
+    if window == "tcp" or window == "arrange" then
+        local rprobj = nil
+        if segment == "track" then
+            rprobj = reaper.BR_GetMouseCursorContext_Track();
+        elseif segment == "envelope" then
+            rprobj, takeenv = reaper.BR_GetMouseCursorContext_Envelope()
+            rprobj = takeenv and nil or rprobj
+        end
+        if rprobj then
+            if SetupSingleElement(rprobj) and #Get_VT_TB() then
+                    local _, v = next(Get_VT_TB())
+                    Show_menu(v)
+            end
+        end
+    end
+end
+
+reaper.atexit(StoreInProject)
+xpcall(Main, GetCrash())
