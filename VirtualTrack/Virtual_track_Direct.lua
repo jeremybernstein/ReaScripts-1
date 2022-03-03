7996ac4ab0457877239c06df944cfd952bdfdc95
Jeremy Bernstein <jeremy.d.bernstein@googlemail.com>
2022-03-03 21:33:34 +0100

    why does whitespace keep getting screwed up here?

--- VirtualTrack/Virtual_track_Direct.lua
+++ VirtualTrack/Virtual_track_Direct.lua
@@ -10,14 +10,14 @@ local reaper = reaper
 package.path = debug.getinfo(1, "S").source:match [[^@?(.*[\/])[^\/]-$]] .. "?.lua;" -- GET DIRECTORY FOR REQUIRE
 
 if not reaper.APIExists("JS_ReaScriptAPI_Version") then
-  reaper.MB( "JS_ReaScriptAPI is required for this script", "Please download it from ReaPack", 0 )
-  return reaper.defer(function() end)
- else
-   local version = reaper.JS_ReaScriptAPI_Version()
-   if version < 1.002 then
-     reaper.MB( "Your JS_ReaScriptAPI version is " .. version .. "\nPlease update to latest version.", "Older version is installed", 0 )
-     return reaper.defer(function() end)
-   end
+    reaper.MB( "JS_ReaScriptAPI is required for this script", "Please download it from ReaPack", 0 )
+    return reaper.defer(function() end)
+else
+    local version = reaper.JS_ReaScriptAPI_Version()
+    if version < 1.002 then
+        reaper.MB( "Your JS_ReaScriptAPI version is " .. version .. "\nPlease update to latest version.", "Older version is installed", 0 )
+        return reaper.defer(function() end)
+    end
 end
 
 require("Modules/VTCommon")
@@ -35,11 +35,9 @@ local function Main()
             rprobj, takeenv = reaper.BR_GetMouseCursorContext_Envelope()
             rprobj = takeenv and nil or rprobj
         end
-        if rprobj then
-            if SetupSingleElement(rprobj) and #Get_VT_TB() then
-                    local _, v = next(Get_VT_TB())
-                    Show_menu(v)
-            end
+        if rprobj and SetupSingleElement(rprobj) and #Get_VT_TB() then
+            local _, v = next(Get_VT_TB())
+            Show_menu(v)
         end
     end
 end
